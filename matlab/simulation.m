f80dbab35b07b2ca5b6e9559d02d8885fb0e32a1
sonson <yoshida.yuichi@gmail.com>
2011-05-02 10:58:34 +0900

    Fixed the problem of rendering 2d code on world coordinates.

--- matlab/simulation.m
+++ matlab/simulation.m
@@ -1,55 +1,56 @@
 function simulation()
 
+% ar condition, focal length and code size
 ar.fx = 1;649.590771179639773*2;
 ar.fy = 1;653.240978126455161*2;
-ar.codeSize = 1;
+ar.codeSize = 0.5;
    
-% 2d code
-p = getRTMatrix([0, pi/3, 0], [0 0 10]);
+% code pose
+p = getRTMatrix([0, pi/6, pi/10], [0 0 2]);
 
-codePositionWorld = cat(1, [-1 1 0;1 1 0;1 -1 0;-1 -1 0]' * ar.codeSize * 0.5, [1 1 1 1]);
+% code original coordinates
+codeOriginalPositionWorld = cat(1, [-1 1 0;1 1 0;1 -1 0;-1 -1 0]' * ar.codeSize * 0.5, [1 1 1 1]);
 
-x = p * codePositionWorld;
+% rotate and translate code by pose matrix
+codePositionWorld = p * codeOriginalPositionWorld;
 
-codeProjectedPosition = project(ar.fx, ar.fy, p * codePositionWorld);
+% projected code into image plane
+codeProjectedPosition = project(ar.fx, ar.fy, codePositionWorld);
 
+% normalize a code's position on image by focal length
 normalizedCodeProjectedPosition = codeProjectedPosition ./ repmat([ar.fx ar.fy]', 1, 4);
 
+% estimate code pose matrix from normalize a code's position on image
 estimatedP = pose_estimation(ar, normalizedCodeProjectedPosition);
 
+% rendering simulation
 img = imread('../resource/code02.png');
 
-x = p * codePositionWorld
-
-drawCode(x, img);
+drawCode(codePositionWorld, img);
 
 hold on;
 
+% exchenge y-z in order to assign z axis to a depth direction 
 line([-0.5 0.5 0.5 -0.5 -0.5], [1 1 1 1 1], [-0.5 -0.5 0.5 0.5 -0.5]);
-
-
-line([0 x(1,1)], [0 x(3,1)], [0 x(2,1)]);
-line([0 x(1,2)], [0 x(3,2)], [0 x(2,2)]);
-line([0 x(1,3)], [0 x(3,3)], [0 x(2,3)]);
-line([0 x(1,4)], [0 x(3,4)], [0 x(2,4)]);
-
-%%fill3([-0.5 0.5 0.5 -0.5], [1 1 1 1], [-0.5 -0.5 0.5 0.5], [1 0 0]);
+line([0 codePositionWorld(1,1)], [0 codePositionWorld(3,1)], [0 codePositionWorld(2,1)]);
+line([0 codePositionWorld(1,2)], [0 codePositionWorld(3,2)], [0 codePositionWorld(2,2)]);
+line([0 codePositionWorld(1,3)], [0 codePositionWorld(3,3)], [0 codePositionWorld(2,3)]);
+line([0 codePositionWorld(1,4)], [0 codePositionWorld(3,4)], [0 codePositionWorld(2,4)]);
 
 drawCode(cat(1, normalizedCodeProjectedPosition*1, [1 1 1 1]), img);
 
-
+daspect([1 1 1]);
 hold off;
-
-view(225, 30);
+view(45, 30);
 
 end
 
 function drawCode(code, img)
-
-x = [[code(1,1) code(1,4)]; [code(1,3) code(1,2)]];
-z = [[code(2,1) code(2,4)]; [code(2,3) code(2,2)]];
-y = [[code(3,1) code(3,4)]; [code(3,3) code(3,2)]];
- 
-warp(x, y, z, img);
-
+% exchenge y-z in order to assign z axis to a depth direction 
+x = [[code(1,1) code(1,2)]; [code(1,4) code(1,3)]];
+z = [[code(2,1) code(2,2)]; [code(2,4) code(2,3)]];
+y = [[code(3,1) code(3,2)]; [code(3,4) code(3,3)]];
+h = surf(x,y,z, img,...
+         'FaceColor','texturemap',...
+         'EdgeColor','none');
 end
