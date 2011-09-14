2656dd020209247dd3c31d2c2e68edb66c780f9d
yyoshida@d-itlab.co.jp <yyoshida@d-itlab.co.jp>
2011-09-14 17:30:20 +0900

    Added UnitTest project for C++.

---
+++ c++/UnitTest/CoreARUnitTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CoreARUnitTest
+//
+//  Created by sonson on 11/09/14.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
