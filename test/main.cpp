ef1735b95b4f3d61ce64078e299abd2dc5d9bf2d
sonson <yoshida.yuichi@gmail.com>
2013-06-08 19:51:59 +0900

    Added eigen and cpputest.

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+#include <CppUTest/CommandLineTestRunner.h>
+
+int main(int argc, char ** argv) {
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+#include <Eigen/Dense>
+#include <CppUTest/TestHarness.h>
+
+#include <CoreAR/CoreAR.h>
+
+TEST_GROUP(CoreAR)
+{};
+
+TEST(CoreAR, CoreARExtractCorners) {
+	LONGS_EQUAL(1, hoge());
+}
